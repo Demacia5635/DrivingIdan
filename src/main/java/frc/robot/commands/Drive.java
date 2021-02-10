b281d448293be6ad5e5e5014526bad3eb7f972f1
YuvalYarmus <yarmusyuval@gmail.com>
2021-02-10 12:13:19 +0200

    added arcade and curvature in the Drive command

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -12,11 +12,17 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Chassis;
+import java.lang.Math.toDegrees; 
+import java.lang.Math.atan; 
+
 
 public class Drive extends CommandBase {
 
   public enum InputHandler{
-    tank,singer,triggerTurns,YandX
+    tank,singer,triggerTurns,YandX, arcadeDrive, curvatureDrive
+  }
+  public enum driveStates {
+    curvatureDrive, arcadeDrive, angularVelocity, radialAccelaration
   }
   private final Chassis chassis;
   private XboxController controller;
@@ -34,6 +40,7 @@ public class Drive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+  
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -61,6 +68,18 @@ public class Drive extends CommandBase {
         chassis.radialAccelaration(velocity, turns);
         break;
     }
+    switch(driveStates) {
+      case arcadeDrive: 
+        zRotation = Math.toDegrees(Math.atan(controller.getY(Hand.kRight)/controller.getX(Hand.kRight))) / 180; 
+        xSpeed = controller.getY(Hand.kLeft); 
+        chassis.arcadeDrive(xSpeed, zRotation, true); 
+      }
+      case curvatureDrive:
+        // TO DO: Decide if and when we want to use isQuickTurn, and what button to put it on
+        zRotation = Math.toDegrees(Math.atan(controller.getY(Hand.kRight)/controller.getX(Hand.kRight))) / 180; 
+        xSpeed = controller.getY(Hand.kLeft); 
+        chassis.curvatureDrive(xSpeed, zRotation, true); 
+
   }
 
   // Called once the command ends or is interrupted.
